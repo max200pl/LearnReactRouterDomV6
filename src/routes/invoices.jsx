a69b350a0680932e5e35ed520647a85176688cc5
max200pl <max2000pl@gmail.com>
2022-06-05 19:01:55 +0300

    Added base stucture routes

--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,28 +1,27 @@
-import * as React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import { getInvoices } from '../data';
 
-export default function App() {
+export default function Invoices() {
   let invoices = getInvoices();
-
   return (
     <div style={{ display: 'flex' }}>
-      <h1>Bookkeeper</h1>
       <nav
         style={{
           borderRight: 'solid 1px',
           padding: '1rem',
         }}
       >
-        {invoices.map((invoices) => (
+        {invoices.map((invoice) => (
           <Link
-            to={`/invoices/${invoices.number}`}
-            style={{ display: 'block0', margin: '1rem 0' }}
+            style={{ display: 'block', margin: '1rem 0' }}
+            to={`/invoices/${invoice.number}`}
+            key={invoice.number}
           >
-            {invoices.name}
+            {invoice.name}
           </Link>
         ))}
       </nav>
+      <Outlet />
     </div>
   );
 }
